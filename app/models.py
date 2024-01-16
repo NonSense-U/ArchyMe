640f78658a7c22aafd3ef275032caf6372b24f97
NonSense <sy5964908@gmail.com>
2024-01-16 14:29:40 -0500

    Users,Posts tables

--- app/models.py
+++ app/models.py
@@ -3,3 +3,24 @@ from sqlalchemy import ForeignKey,String,Column,Integer,Boolean
 from sqlalchemy.sql.sqltypes import TIMESTAMP
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql.expression import text
+
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer,primary_key=True,index=True)
+    username = Column(String(255),unique=True,nullable=False,index=True)
+    email = Column(String(255),unique=True,nullable=False,index=True)
+    hashed_password = Column(String(255),nullable=False)
+    is_active = Column(Boolean,server_default=("True"))
+    is_superuser = Column(Boolean,server_default=("False"))
+    created_at = Column(TIMESTAMP(timezone=True),server_default=text('now()'))
+
+class Post(Base):    
+    __tablename__ = "posts"
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String(255),nullable=False)
+    content = Column(String(255),nullable=False)
+    published = Column(Boolean, server_default=("True"))
+    created_at = Column(TIMESTAMP(timezone=True), server_default=text('now()'))
+    owner_id = Column(Integer, ForeignKey('users.id',ondelete=("CASCADE")))
+    owner = relationship("User")
