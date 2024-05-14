9e04b66b1533fec91d0dbed1599f7c63843a3247
NonSense <sy5964908@gmail.com>
2024-05-14 20:31:01 +0300

    Adding CORSMiddleware

--- app/main.py
+++ app/main.py
@@ -1,9 +1,18 @@
 from fastapi import FastAPI
 from .routers import Follow, Reaction, Accounts,Posts,Auth
+from fastapi.middleware.cors import CORSMiddleware
 
+origins  = ["*"]
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(Accounts.router)
 app.include_router(Posts.router)
